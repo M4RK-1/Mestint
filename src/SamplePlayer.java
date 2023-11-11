818c07f2fe3c5ad319bbd0ecbdb3dec31388938a
M4RK-1 <vizimark@gmail.com>
2023-11-11 15:41:15 +0100

    Working 1 distance step without coins with best order of points to pick up

--- src/SamplePlayer.java
+++ src/SamplePlayer.java
@@ -5,10 +5,9 @@ import game.racetrack.utils.Cell;
 import game.racetrack.utils.Coin;
 import game.racetrack.utils.PlayerState;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Random;
+import java.util.*;
+
+import static java.lang.Math.abs;
 
 public class SamplePlayer extends RaceTrackPlayer {
 
@@ -80,15 +79,148 @@ public class SamplePlayer extends RaceTrackPlayer {
 
     public SamplePlayer(PlayerState state, Random random, int[][] track, Coin[] coins, int color) {
         super(state, random, track, coins, color);
+
+        int[][] findPath = new int[myTrack.length][myTrack[0].length];
+        for (int i = 0; i < myTrack.length; i++) {
+            for (int j = 0; j < myTrack[i].length; j++) {
+                if (myTrack[i][j] == 1) {
+                    findPath[i][j] = 0;
+                } else if (myTrack[i][j] == 2) {
+                    findPath[i][j] = -100;
+                } else if (myTrack[i][j] == 5) {
+                    findPath[i][j] = -12;
+                } else if (myTrack[i][j] == 33) {
+                    findPath[i][j] = 0;
+                } else {
+                    findPath[i][j] = -100;
+                }
+                findPath[state.i][state.j] = -1;
+            }
+        }
+        for (int j = 0; j < coins.length; j++) {
+            findPath[coins[j].i][coins[j].j]=((j+2)*-1);
+        }
+
+        /*for (int i = 0; i < findPath.length; i++) {
+            for (int j = 0; j < findPath[i].length; j++) {
+                switch (findPath[i][j]) {
+                    case 0 -> System.out.print(" ");
+                    case -100 -> System.out.print("X");
+                    default -> System.out.print(Math.abs(findPath[i][j] % 10) + "");
+                }
+            }
+            System.out.println();
+        }*/
+
+        for (int actualNumber = -1; actualNumber > -13; actualNumber--) {
+            GetAllDistances(findPath,actualNumber);
+        }
+        /*int numVertices = distanceMatrix.length;
+        int startVertex = 0; // Replace with your fixed starting vertex
+        int endVertex = 11; // Replace with your fixed ending vertex
+        int numRemainingVertices = numVertices - 1;
+
+        List<Integer> bestPath = null;
+        int shortestDistance = Integer.MAX_VALUE;
+
+        // Generate all possible permutations of 10 remaining vertices (excluding start and end)
+        int[] permutation = new int[10];
+        for (int i1 = 1; i1 < numRemainingVertices; i1++) {
+            permutation[0] = i1;
+            for (int i2 = 1; i2 < numRemainingVertices; i2++) {
+                permutation[1] = i2;
+                for (int i3 =  1; i3 < numRemainingVertices; i3++) {
+                    permutation[2] = i3;
+                    for (int i4 =  1; i4 < numRemainingVertices; i4++) {
+                        permutation[3] = i4;
+                        for (int i5 =  1; i5 < numRemainingVertices; i5++) {
+                            permutation[4] = i5;
+                            for (int i6 =  1; i6 < numRemainingVertices; i6++) {
+                                permutation[5] = i6;
+                                for (int i7 =  1; i7 < numRemainingVertices; i7++) {
+                                    permutation[6] = i7;
+                                    for (int i8 =  1; i8 < numRemainingVertices; i8++) {
+                                        permutation[7] = i8;
+                                        for (int i9 =  1; i9 < numRemainingVertices; i9++) {
+                                            permutation[8] = i9;
+                                            for (int i10 =  1; i10 < numRemainingVertices; i10++) {
+                                                permutation[9] = i10;
+
+                                                // Create a path by adding the fixed start and end vertices
+                                                List<Integer> currentPath = new ArrayList<>();
+                                                currentPath.add(startVertex);
+                                                for (int vertex : permutation) {
+                                                    currentPath.add(vertex);
+                                                }
+                                                currentPath.add(endVertex);
+
+                                                int currentDistance = calculateTotalDistance(currentPath, distanceMatrix);
+
+                                                if (currentDistance < shortestDistance) {
+                                                    shortestDistance = currentDistance;
+                                                    bestPath = currentPath;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+        */
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<int[]> permutations = new ArrayList<>();
+        generatePermutations(nums, 0, permutations);
+
+        int midIndex = (permutations.size() + 1) / 2;
+
+
+        int shortestDistance = Integer.MAX_VALUE;
+
+        for (int[] permutation : permutations) {
+            // Create a path by adding the fixed start and end vertices
+            List<Integer> currentPath = new ArrayList<>();
+            currentPath.add(0);
+            for (int num: permutation) {
+                currentPath.add(num);
+            }
+            currentPath.add(11);
+
+            int currentDistance = calculateTotalDistance(currentPath, distanceMatrix);
+
+            if (currentDistance < shortestDistance) {
+                shortestDistance = currentDistance;
+                bestPath = currentPath;
+            }
+        }
+
+        //System.out.println("Path: " + bestPath);System.out.println("Total Distance: " + shortestDistance);
+
     }
 
 
+
+
     public int[][] myTrack = track;
+
+    public int[][] distanceMatrix = new int[12][12];
+
+    List<Integer> bestPath = null;
 
     public boolean findRoute=true;
 
     public ArrayList<Integer> moveList = new ArrayList<>();
     public int moveListCounter = -1;
+
+
 
 
     public game.racetrack.utils.Cell lastPosition = toCell(state);
@@ -112,12 +244,15 @@ public class SamplePlayer extends RaceTrackPlayer {
         }
 
 
-        /*if (random.nextInt(100) < 3) {
+        /*
+        //randomlep
+        if (random.nextInt(100) < 3) {
             lastPosition = toCell(state);
             return RaceTrackGame.DIRECTIONS[random.nextInt(RaceTrackGame.DIRECTIONS.length)];
         }*/
 
         if (findRoute){
+
             //region mapcopy
             PositionWithParent[][] findPath = new PositionWithParent[myTrack.length][myTrack[0].length];
             for (int i = 0; i < myTrack.length; i++) {
@@ -141,7 +276,7 @@ public class SamplePlayer extends RaceTrackPlayer {
             }
             //endregion
 
-
+            /*
             //region kozeli lepes
             for (int i = 0; i < neighbors.length; i++) {
                 int newRow = this.state.i + neighbors[i][0];
@@ -154,6 +289,8 @@ public class SamplePlayer extends RaceTrackPlayer {
                 }
             }
             //endregion
+            */
+
 
 
             //region middle line generate
@@ -237,9 +374,9 @@ public class SamplePlayer extends RaceTrackPlayer {
 
             for (int i = 0; i < findMiddleLine.length; i++) {
                 for (int j = 0; j < findMiddleLine[0].length; j++) {
-                    if (findMiddleLine[i][j].value < 3 && findMiddleLine[i][j].value > 0) {
+                    if (findMiddleLine[i][j].value == 1) {
                         findMiddleLine[i][j].value = -1;
-                    } else if (findMiddleLine[i][j].value >= 3) {
+                    } else if (findMiddleLine[i][j].value >= 2) {
                         findMiddleLine[i][j].value = 0;
                     } else {
                         findMiddleLine[i][j].value = -1;
@@ -288,6 +425,8 @@ public class SamplePlayer extends RaceTrackPlayer {
 
             findPath = findMiddleLine;
 
+            //PrintPathMap(findPath);
+
             PositionWithParent kovetkezoCel = new PositionWithParent(0, 0, 0);
 
 
@@ -310,7 +449,9 @@ public class SamplePlayer extends RaceTrackPlayer {
 
                                     if (findPath[newRow][newCol].value == 0 || findPath[newRow][newCol].value > c + 1)
                                         findPath[newRow][newCol].value = c + 1;
-                                    else if (findPath[newRow][newCol].value == -2 /*|| findPath[newRow][newCol].value == -3*/) {
+                                    else if (findPath[newRow][newCol].value == -2
+                                        //|| findPath[newRow][newCol].value == -3
+                                    ) {
                                         kovetkezoCel = findPath[newRow][newCol];
                                         break outerLoop;
                                     }
@@ -375,6 +516,8 @@ public class SamplePlayer extends RaceTrackPlayer {
             int[] vectorStartingPosition = {state.i, state.j};
             vectorParsing[vectorStartingPosition[0]][vectorStartingPosition[1]].value = 2;
 
+            //endregion
+
             //region elek kerekitese
             int[][] vertCrossNeighbors = {
                     {-1, 0}, {1, 0}
@@ -414,7 +557,7 @@ public class SamplePlayer extends RaceTrackPlayer {
                 }
             }
             //endregion
-            //endregion
+
 
 
 
@@ -667,6 +810,88 @@ public class SamplePlayer extends RaceTrackPlayer {
 
     public Cell toCell(PlayerState ps) {
         return new Cell(ps.i, ps.j);
+    }
+
+    public static void generatePermutations(int[] nums, int index, List<int[]> result) {
+        if (index == nums.length) {
+            result.add(Arrays.copyOf(nums, nums.length));
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            int temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+
+            generatePermutations(nums, index + 1, result);
+
+            temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
+    public static int calculateTotalDistance(List<Integer> path, int[][] distanceMatrix) {
+        int totalDistance = 0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            totalDistance += distanceMatrix[path.get(i)][path.get(i + 1)];
+        }
+        return totalDistance;
+    }
+
+    private void GetAllDistances(int[][] findPath,int actualNumber) {
+
+        int[][] distanceMap = new int[myTrack.length][myTrack[0].length];
+        for (int i = 0; i < findPath.length; i++) {
+            for (int j = 0; j < findPath[i].length; j++) {
+                distanceMap[i][j] = findPath[i][j];
+            }
+        }
+
+        ArrayList<Integer> erintettCelok=new ArrayList<>();
+        erintettCelok.add(actualNumber);
+        distanceMatrix[abs(actualNumber)-1][abs(actualNumber)-1]=0;
+
+        int c = 1;
+        outerLoop:
+        for (c = 1; c < 1000; c++) {
+
+
+            for (int i = 0; i < distanceMap.length; i++) {
+                for (int j = 0; j < distanceMap[0].length; j++) {
+                    if (distanceMap[i][j] == actualNumber || (distanceMap[i][j]==c-1&&distanceMap[i][j] != actualNumber&&distanceMap[i][j] !=0)) {
+                        for (int[] neighbor : neighbors) {
+                            int newRow = i + neighbor[0];
+                            int newCol = j + neighbor[1];
+                            try {
+                                if (distanceMap[newRow][newCol] == 0)
+                                    distanceMap[newRow][newCol] = c;
+                                else if (distanceMap[newRow][newCol] <0 && distanceMap[newRow][newCol]!=-100 && !erintettCelok.contains(distanceMap[newRow][newCol])) {
+                                    erintettCelok.add(distanceMap[newRow][newCol]);
+                                    distanceMatrix[abs(actualNumber)-1][abs(distanceMap[newRow][newCol])-1]=c;
+                                    /*
+                                    //kiir
+                                    for (int[] ints : distanceMap) {
+                                        for (int anInt : ints) {
+                                            switch (anInt) {
+                                                case 0 -> System.out.print(" ");
+                                                case -100 -> System.out.print(" ");
+                                                default -> System.out.print(abs(anInt % 10) + "");
+                                            }
+                                        }
+                                        System.out.println();
+                                    }
+                                    */
+                                }
+                            } catch (Exception ignored) {
+                            }
+
+
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
